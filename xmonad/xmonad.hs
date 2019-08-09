c4750998bd14aa36540c9483ae64a0710c0333b5
josesl <jose.sanleandro@osoco.es>
2019-08-09 20:12:20 +0200

    Updated config from jamacuco

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,22 @@
 -- xmonad config used by Vic Fryzel
 -- Author: Vic Fryzel
--- http://github.com/vicfryzel/xmonad-config
+-- https://github.com/vicfryzel/xmonad-config
+
 import System.IO
 import System.Exit
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import Graphics.X11.ExtraTypes.XF86
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
@@ -15,15 +26,33 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
---myTerminal = "/nix/var/nix/profiles/default/bin/terminator"
-myTerminal = "/nix/var/nix/profiles/default/bin/tilda"
+myTerminal = "/home/chous/bin/term.sh"
+
+-- The command to lock the screen or show the screensaver.
+myScreensaver = "/run/current-system/sw/bin/xscreensaver-command -lock; xset dpms force off"
+
+-- The command to take a selective screenshot, where you select
+-- what you'd like to capture on the screen.
+mySelectScreenshot = "select-screenshot"
+
+-- The command to take a fullscreen screenshot.
+myScreenshot = "screenshot"
+
+-- The command to use as a launcher, to launch commands that don't have
+-- preset keybindings.
+-- myLauncher = "$(yeganesh -x -- -fn 'monospace-8' -nb '#000000' -nf '#FFFFFF' -sb '#7C7C7C' -sf '#CEFFAC')"
+myLauncher = "dmenu_run -b -nb black"
+
+-- Location of your xmobar.hs / xmobarrc
+myXmobarrc = "~/.xmonad/xmobar-single.hs"
 
 
 ------------------------------------------------------------------------
 -- Workspaces
 -- The default number of workspaces (virtual screens) and their names.
 --
-myWorkspaces = ["1:term","2:pharo","3:firefox","4:chromim","5:tor","6:win","7:java"] ++ map show [6..9]
+myWorkspaces = ["1:term","2:emacs","3:firefox","4:chrome","5:pharo","6:misc","7:idea"] ++ map show [6..9]
+
 
 ------------------------------------------------------------------------
 -- Window rules
@@ -40,47 +69,70 @@ myWorkspaces = ["1:term","2:pharo","3:firefox","4:chromim","5:tor","6:win","7:ja
 -- 'className' and 'resource' are used below.
 --
 myManageHook = composeAll
-    [ className =? "Firefox"        --> doShift "3:firefox"
-    , className =? "Emacs"          --> doShift "2:pharo"
-    , className =? "emacs"          --> doShift "2:pharo"
-    , className =? "pharo-vm"       --> doShift "2:pharo"
-    , className =? "chromium"       --> doShift "4:chromium"
-    , className =? "Chromium"       --> doShift "4:chromium"
-    , className =? "Google-chrome"  --> doShift "4:chromium"
-    , className =? "Tor"            --> doShift "5:tor"
-    , className =? "VirtualBox"     --> doShift "6:win"
-    , className =? "jetbrains-idea" --> doShift "7:java"
-    , className =? "org-apache-maven-surefire-booter-ForkedBooter" --> doShift "1:term"
-    , className =? "Thunderbird"    --> doShift "3:firefox"
-    , resource  =? "desktop_window" --> doIgnore
-    , className =? "Gimp"           --> doFloat
-    , className =? "Skype"          --> doFloat
-    , resource  =? "gpicview"       --> doFloat
-    , className =? "MPlayer"        --> doFloat
-    , className =? "VirtualBox"     --> doShift "1:term"
-    , className =? "Xchat"          --> doShift "4:chromium"
-    , className =? "stalonetray"    --> doIgnore
-    , className =? "Pidgin"         --> doShift "1:term"
-    , className =? "trayer"         --> doIgnore
-    , resource  =? "trayer"         --> doIgnore
-    ]
+    [ className =? "Emacs"            --> doShift "2:emacs"
+    , className =? "Firefox"          --> doShift "3:firefox"
+    , className =? "Chromium-browser" --> doShift "4:chrome"
+    , className =? "Slack"            --> doShift "4:chrome"
+    , className =? "pharo-vm"         --> doShift "5:pharo"
+    , resource  =? "desktop_window"   --> doIgnore
+    , className =? "Steam"            --> doFloat
+    , className =? "Gimp"             --> doFloat
+    , resource  =? "gpicview"         --> doFloat
+    , className =? "MPlayer"          --> doFloat
+    , className =? "VirtualBox"       --> doShift "6:misc"
+    , className =? "stalonetray"      --> doIgnore
+    , className =? "jetbrains-idea"   --> doShift "7:idea"
+    , className =? "robo3t"           --> doShift "6:misc"
+    , className =? "calibre"          --> doShift "6:misc"
+    , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
+
+
+------------------------------------------------------------------------
+-- Layouts
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+-- The available layouts.  Note that each layout is separated by |||,
+-- which denotes layout choice.
+--
+myLayout = avoidStruts (
+    ThreeColMid 1 (3/100) (1/2) |||
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2)) |||
+    tabbed shrinkText tabConfig |||
+    Full |||
+    spiral (6/7)) |||
+    noBorders (fullscreenFull Full)
 
 
 ------------------------------------------------------------------------
 -- Colors and borders
 -- Currently based on the ir_black theme.
 --
-myNormalBorderColor  = "#AcAcAc"
-myFocusedBorderColor = "#00b6b0"
+myNormalBorderColor  = "#7c7c7c"
+myFocusedBorderColor = "#ffb6b0"
+
+-- Colors for text and backgrounds of each tab when in "Tabbed" layout.
+tabConfig = defaultTheme {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
 
 -- Color of current window title in xmobar.
-xmobarTitleColor = "#00B6B0"
+xmobarTitleColor = "#FFB6B0"
 
 -- Color of current workspace in xmobar.
 xmobarCurrentWorkspaceColor = "#CEFFAC"
 
 -- Width of the window border in pixels.
-myBorderWidth = 3 
+myBorderWidth = 3
+
 
 ------------------------------------------------------------------------
 -- Key bindings
@@ -90,6 +142,11 @@ myBorderWidth = 3
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
+-- myModMask = mod1Mask -- Alt_L
+-- myModMask = mod2Mask -- No idea which key is.
+-- myModMask = mod3Mask -- No idea which key is.
+-- myModMask = mod4Mask -- Super_L (Win)
+-- myModMask = mod5Mask -- No idea which key is.
 myModMask = mod4Mask
 
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
@@ -101,29 +158,34 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   [ ((modMask .|. shiftMask, xK_Return),
      spawn $ XMonad.terminal conf)
 
-  -- Lock the screen using xscreensaver.
+  -- Lock the screen using command specified by myScreensaver.
   , ((modMask .|. controlMask, xK_l),
-     spawn "xscreensaver-command -lock")
+     spawn myScreensaver)
 
-  -- Launch dmenu via yeganesh.
+  -- Spawn the launcher using command specified by myLauncher.
   -- Use this to launch programs without a key binding.
---  , ((modMask, xK_p),
---     spawn "dmenu-with-yeganesh")
+  , ((modMask, xK_p),
+     spawn myLauncher)
 
-  -- Take a screenshot in select mode.
-  -- After pressing this key binding, click a window, or draw a rectangle with
-  -- the mouse.
-  , ((modMask .|. shiftMask, xK_p),
-     spawn "select-screenshot")
+  -- Take a selective screenshot using the command specified by mySelectScreenshot.
+  , ((modMask .|. shiftMask, xK_y),
+     spawn mySelectScreenshot)
 
-  -- Take full screenshot in multi-head mode.
-  -- That is, take a screenshot of everything you see.
-  , ((modMask .|. controlMask .|. shiftMask, xK_p),
-     spawn "screenshot")
+  -- Take a full screenshot using the command specified by myScreenshot.
+  , ((modMask .|. controlMask .|. shiftMask, xK_y),
+     spawn myScreenshot)
 
-  -- Fetch a single use password.
-  , ((modMask .|. shiftMask, xK_o),
-     spawn "fetchotp -x")
+  -- Mute volume.
+  , ((0, xF86XK_AudioMute),
+     spawn "amixer -q set Master toggle")
+
+  -- Decrease volume.
+  , ((0, xF86XK_AudioLowerVolume),
+     spawn "amixer -q set Master 5%-")
+
+  -- Increase volume.
+  , ((0, xF86XK_AudioRaiseVolume),
+     spawn "amixer -q set Master 5%+")
 
   -- Mute volume.
   , ((modMask .|. controlMask, xK_m),
@@ -131,11 +193,11 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
   -- Decrease volume.
   , ((modMask .|. controlMask, xK_j),
-     spawn "amixer -q set Master 10%-")
+     spawn "amixer -q set Master 5%-")
 
   -- Increase volume.
   , ((modMask .|. controlMask, xK_k),
-     spawn "amixer -q set Master 10%+")
+     spawn "amixer -q set Master 5%+")
 
   -- Audio previous.
   , ((0, 0x1008FF16),
@@ -227,11 +289,11 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- keybindings for programs
   , ((modMask, xK_e), spawn "emacs")
   , ((modMask, xK_f), spawn "firefox")
-  , ((modMask, xK_p), spawn "pidgin")
+--  , ((modMask, xK_p), spawn "pidgin")
   , ((modMask, xK_c), spawn "chromium")
   , ((modMask, xK_g), spawn "gimp")
   , ((modMask, xK_b), spawn "blender")
-  , ((modMask, xK_y), spawn "skype")
+  , ((modMask, xK_i), spawn "idea-ultimate")
 
   -- Quit xmonad.
   , ((modMask .|. shiftMask, xK_q),
@@ -241,11 +303,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((modMask, xK_q),
      restart "xmonad" True)
 
-  , ((mod4Mask, xK_q), spawn "cd ~/.xmonad; nix-shell --pure --command 'ghc --make xmonad.hs -i -ilib -fforce-recomp -v0 -o xmonad-x86_64-linux' && xmonad --restart")
+  , ((modMask, xK_q), spawn "cd ~/.xmonad, nix-shell --pure --command 'ghc --make xmonad.hs -i -ilib -fforce-recomp -v0 -o xmonad-x86_64-linux' && xmonad --restart")
 
-  -- resizing the master/slave ratio
-  , ((modMask, xK_i     ), sendMessage Shrink) -- %! Shrink the master area
-  , ((modMask, xK_d     ), sendMessage Expand) -- %! Expand the master area
   ]
   ++
 
@@ -256,10 +315,10 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
   ++
 
-  -- mod-{a,o,n,s}, Switch to physical/Xinerama screens 1, 2, 3 or 4
-  -- mod-shift-{a,o,n,s}, Move client to screen 1, 2, 3 or 4
+  -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+  -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
   [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_o, xK_n, xK_s, xK_a] [0..]
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
@@ -298,6 +357,7 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 --
 -- > logHook = dynamicLogDzen
 --
+--
 
 
 ------------------------------------------------------------------------
@@ -307,14 +367,57 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 -- per-workspace layout choices.
 --
 -- By default, do nothing.
-myStartupHook = setWMName "LG3D"
-
+myStartupHook = return ()
 
 ------------------------------------------------------------------------
 -- Run xmonad with all the defaults we set up.
 --
 main = do
-  xmonad $ defaults
+  xmproc <- spawnPipe ("xmobar " ++ myXmobarrc)
+  xmonad $ defaults {
+        logHook = dynamicLogWithPP $ defaultPP
+
+      -- display current workspace as darkgrey on light grey (opposite of 
+      -- default colors)
+         {  ppCurrent         = dzenColor "#303030" "#909090" . pad 
+
+         -- display other workspaces which contain windows as a brighter grey
+          , ppHidden          = dzenColor "#909090" "" . pad 
+
+         -- display other workspaces with no windows as a normal grey
+          , ppHiddenNoWindows = dzenColor "#606060" "" . pad 
+   
+         -- display the current layout as a brighter grey
+          , ppLayout          = dzenColor "#909090" "" . pad 
+
+         -- if a window on a hidden workspace needs my attention, color it so
+          , ppUrgent          = dzenColor "#ff0000" "" . pad . dzenStrip
+
+         -- shorten if it goes over 100 characters
+          , ppTitle           = shorten 100
+
+        -- no separator between workspaces
+          , ppWsSep           = ""
+
+        -- put a few spaces between each object
+          , ppSep             = "  "
+
+        -- output to the handle we were given as an argument
+--          , ppOutput          = hPutStrLn h
+        }
+---      logHook = h
+--              = dynamicLogWithPP $ xmobarPP {
+--            ppOutput = hPutStrLn xmproc
+--          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+--          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+--          , ppSep = "   "
+--      }
+      , manageHook = manageDocks <+> myManageHook
+--      , startupHook = docksStartupHook <+> setWMName "LG3D"
+      , startupHook = setWMName "LG3D"
+      , handleEventHook = docksEventHook
+  }
+
 
 ------------------------------------------------------------------------
 -- Combine it all together
@@ -324,7 +427,7 @@ main = do
 --
 -- No need to modify this.
 --
-defaults = ewmh defaultConfig {
+defaults = defaultConfig {
     -- simple stuff
     terminal           = myTerminal,
     focusFollowsMouse  = myFocusFollowsMouse,
@@ -339,9 +442,7 @@ defaults = ewmh defaultConfig {
     mouseBindings      = myMouseBindings,
 
     -- hooks, layouts
+    layoutHook         = smartBorders $ myLayout,
     manageHook         = myManageHook,
-    startupHook        = myStartupHook,
-
-    -- workarounds
-    handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+    startupHook        = myStartupHook
 }
